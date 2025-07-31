e7eecf291ec9b1553dcabfbff47f687f45c8be8b
Adefisayomi <ariyoadefisayomi@gmail.com>
2025-07-31 01:04:33 +0100

    added supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://pqfgugwebejzasooservd.supabase.co";
-const supabaseKey = "sb_publishable_mIUbG9eB2Ed4r5EwLtAq0w_zrGdOVBP";
+const supabaseUrl = "https://pqfugwebejzasosoervd.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBxZnVnd2ViZWp6YXNvc29lcnZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5MTE2OTgsImV4cCI6MjA2OTQ4NzY5OH0.wSEJvhLiJx4a0XfLmFi_x1zgtDFG6jyhQjWjn9FHlcU";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
