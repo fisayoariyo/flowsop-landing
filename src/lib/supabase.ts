bfb9f929dba7701c19628a20e9cc6231b56b6988
Adefisayomi <ariyoadefisayomi@gmail.com>
2025-07-31 12:21:06 +0100

    made admin page inaccessible in supabase too

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,4 +4,9 @@ const supabaseUrl = "https://tsjqxaaicygtdprxljpg.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzanF4YWFpY3lndGRwcnhsanBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5MTc3MDEsImV4cCI6MjA2OTQ5MzcwMX0.TvfxrFspVgRISRTMxy3xVV1Nif69CyDBiN2OuXQ8VEc";
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true, // keep user logged in across reloads
+    detectSessionInUrl: true, // handle OAuth/magic link redirects automatically
+  },
+});
