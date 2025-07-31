c4b663c8c8ded52ed53cba14ce99a6ef935b9e88
Adefisayomi <ariyoadefisayomi@gmail.com>
2025-07-31 01:29:25 +0100

    edited supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://pqfugwebejzasosoervd.supabase.co";
+const supabaseUrl = "https://tsjqxaaicygtdprxljpg.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBxZnVnd2ViZWp6YXNvc29lcnZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5MTE2OTgsImV4cCI6MjA2OTQ4NzY5OH0.wSEJvhLiJx4a0XfLmFi_x1zgtDFG6jyhQjWjn9FHlcU";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzanF4YWFpY3lndGRwcnhsanBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5MTc3MDEsImV4cCI6MjA2OTQ5MzcwMX0.TvfxrFspVgRISRTMxy3xVV1Nif69CyDBiN2OuXQ8VEc";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
